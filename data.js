06ef55620d113fc244f617804e08cd98afb62ded
AyushAwasthi2384 <ayushawasthicoding@gmail.com>
2024-01-01 18:13:12 +0530

    first last

---
+++ data.js
@@ -0,0 +1,12 @@
+// var user = {
+//     username: "admin",
+//     password: "admin123"
+// }
+
+var temp = {
+    frname: document.form.fname.value,
+    lsname: document.form.lname.value,
+    username: document.form.userName.value,
+    password: document.form.password.value
+}
+
---
+++ script.js
@@ -0,0 +1,7 @@
+var uname = document.form.userName.value
+var pass = document.form.password.value
+console.log(uname+pass)
+
+document.querySelector("#login-btn").addEventListener('click', ()=> {
+    console.log(temp)
+});
